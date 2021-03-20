8c3fde02aee38f803fe82886d934f93ed76cbc03
Vindhya R <vindhyarao2013@gmail.com>
2021-03-20 11:52:31 -0400

    Update settings.py

--- ddt_api/settings.py
+++ ddt_api/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'dontforgetit',
+        'PASSWORD': '******',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
